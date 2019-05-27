c1faffd743d2b401474d406b07d7d11a93e41a8a
quaiyumlamisa <46520507+quaiyumlamisa@users.noreply.github.com>
2019-05-27 23:23:05 +0600

    Add files via upload

--- h.c
+++ h.c
@@ -6,6 +6,8 @@
 
 #define MAX_WORDS   1000
 #define MAX_FILE_NAME 100
+#define MAX 256
+
 void clrscr(void)
 {
 	system("clear");
@@ -26,7 +28,7 @@ void Create()
         printf("Enter the name of the file:\n");
         scanf("%s",&ch);
  fp1=fopen(ch,"w");
-    printf("%s",ch);
+
 
     printf("\n\tEnter the text and press '*' to save\n\n\t");
 
@@ -135,6 +137,88 @@ void Create()
 
 }
 
+void CopyFile()
+    {
+    printf("\n\tEnter the new filename to copy:  ");
+    scanf("%s",fn);
+     printf("\n\tEnter the previous filename to copy:  ");
+      scanf("%s",fn1);
+    fp1=fopen(fn1,"r");
+    fp2=fopen(fn,"w");
+    while(!feof(fp1))
+    {
+    c=getc(fp1);
+
+    putc(c,fp2);
+    }
+    fclose(fp2);
+    }
+
+
+
+void deleteLine()
+{
+
+
+
+
+
+        int lno, ctr = 0;
+        char ch;
+        FILE *fptr1, *fptr2;
+		char fname[MAX];
+        char str[MAX], temp[] = "temp.txt";
+		printf("\n\n Delete a specific line from a file :\n");
+		printf("-----------------------------------------\n");
+		printf(" Input the file name to be opened : ");
+		scanf("%s",fname);
+        fptr1 = fopen(fname, "r");
+        if (!fptr1)
+		{
+                printf(" File not found or unable to open the input file!!\n");
+                return 0;
+        }
+        fptr2 = fopen(temp, "w"); // open the temporary file in write mode
+        if (!fptr2)
+		{
+                printf("Unable to open a temporary file to write!!\n");
+                fclose(fptr1);
+                return 0;
+        }
+        printf(" Input the line you want to remove : ");
+        scanf("%d", &lno);
+		lno++;
+        // copy all contents to the temporary file except the specific line
+        while (!feof(fptr1))
+        {
+            strcpy(str, "\0");
+            fgets(str, MAX, fptr1);
+            if (!feof(fptr1))
+            {
+                ctr++;
+                /* skip the line at given line number */
+                if (ctr != lno)
+                {
+                    fprintf(fptr2, "%s", str);
+                }
+            }
+        }
+        fclose(fptr1);
+        fclose(fptr2);
+        remove(fname);
+        rename(temp, fname);
+
+   fptr1=fopen(fname,"r");
+            ch=fgetc(fptr1);
+
+        fclose(fptr1);
+
+
+  }
+
+
+
+
 void CopyLine()
 {
     int numOfLineToCopy,numOfLineToPaste;
@@ -242,19 +326,48 @@ void Append(char *line)
 
   char fname[20];
 
-  char s[10]="</i>";
+ // char s[10]="</i>";
+    char s[10]="</i>";
     char p[10]="<i>";
 
-  printf("%s\n",line);
+ // printf("%s\n",line);
   strcat(line,s);
-   printf("%s\n",line);
+ //  printf("%s\n",line);
 
    strcat(p,line);
    printf("%s",p);
 
 
+/*
+
+char ch[100]="</i>",ch2[100];
 
 
+
+char faltu[100];
+
+strcpy(faltu,p);
+
+printf("%s",faltu);
+
+        int k=0;
+
+
+        for(int j=0;faltu[j]!=0;j++,k++)
+        {
+            ch2[k] = faltu[j];
+        }
+
+        for(int i=0;ch[i]!='\0';i++,k++)
+        {
+            ch2[k]=ch[i];
+        }
+
+        ch2[k]='\0';
+
+        printf("%s",ch2);
+
+*/
          printf("Enter file name: ");
     scanf("%s", fn);
 
@@ -266,9 +379,19 @@ void Append(char *line)
 
 
     fclose (fp);
+
+
     }
 
-
+char* replace_char(char* str, char find, char replace)
+{
+    char *current_pos=strchr(str,find);
+    while(current_pos) {
+        *current_pos = replace;
+        current_pos = strchr(current_pos, find);
+    }
+    return str;
+}
 
 void manipulation()
 {
@@ -294,6 +417,7 @@ printf("\nEnter the line to mnplt : ");
         if(i == numOfLineToCopy )
         {
             printf("%s\n", line);
+            replace_char(line, '\n', ' ');
              Append(line);
         }
     }
@@ -304,17 +428,25 @@ printf("\nEnter the line to mnplt : ");
 
 
 }
+
+
 int main(void)
 {
-       // Create();
-       Create_html();
-          Display();
-          Display();
+      //  Create();
+        //Create_html();
+        //printf("display the created txt file\n");
+        Display();
+       //printf("display the created html(txt) file\n");
+        Display();
 
        // Count();
         manipulation();
-         Display();
+        Display();
          CopyLine();
+         Display();
+         deleteLine();
+         Display();
+         CopyFile();
          Display();
         return 0;
 }
